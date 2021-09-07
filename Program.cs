6989d5e9b2f37862f2c0848ebba9d01f04698e95
Paramesh Muthaiyan <88918337+ParameshMuthaiyan@users.noreply.github.com>
2021-09-07 09:54:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orthogonality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ip1 = Console.ReadLine().Split(' ');
+            string[] ip2 = Console.ReadLine().Split(' ');
+            int productAB=0;
+            for (int i = 0; i < n; i++)
+            {
+                int x = Convert.ToInt32(ip1[i]);
+                int y = Convert.ToInt32(ip2[i]);
+                productAB += (x * y);
+            }
+            if (productAB == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
