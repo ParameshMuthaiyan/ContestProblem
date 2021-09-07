ae6401ad25d418f83b54a361f69120f00ac636ed
Paramesh Muthaiyan <88918337+ParameshMuthaiyan@users.noreply.github.com>
2021-09-07 16:20:20 +0530

    Add files via upload

---
+++ GradeBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] markList = new float[5];
+            for (int i = 0; i < markList.Length; i++)
+            {
+                markList[i] = Convert.ToSingle(Console.ReadLine());
+            }
+            Statistics s = new Statistics();
+            Console.WriteLine(s.Avg(markList));
+            Console.WriteLine(s.Maximum(markList));
+            Console.WriteLine(s.Minimum(markList));
+        }
+      
+     }
+}
---
+++ Statistics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Statistics
+    {
+        //float[] mark;
+        //public Statistics(float[] markList)
+        //{
+        //    this.mark = markList;
+        //}
+        //public float[] mark
+        //{
+        //    get { return mark; }
+        //    set { mark = value; }
+        //}
+
+        public float Avg(float[] markList)
+        {
+            float sum = 0;
+            for (int i = 0; i < markList.Length; i++)
+            {
+                sum += markList[i];
+            }
+            return sum / markList.Length;
+        }
+
+        public float Maximum(float[] markList)
+        {
+            float max = markList[0];
+            for (int i = 0; i < markList.Length; i++)
+            {
+                max = Math.Max(max, markList[i]);
+            }
+            return max;
+        }
+        public float Minimum(float[] markList)
+        {
+            float min = markList[0];
+            for (int i = 0; i < markList.Length; i++)
+            {
+                min = Math.Min(min, markList[i]);
+            }
+            return min;
+        }
+
+
+
+    }
+
+
+
+}
